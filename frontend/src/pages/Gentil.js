98df565b1405412e8c3b1029497fb1c86db806e2
Sawlyer <160416029+Sawlyer@users.noreply.github.com>
2025-04-09 16:05:44 +0200

    PATCH: Titouan vs AI : 0-1

--- frontend/src/pages/Gentil.js
+++ frontend/src/pages/Gentil.js
@@ -4,50 +4,48 @@ import { useNavigate } from 'react-router-dom';
 import "./Gentil.css"
 
 function Gentil()  {
+  const navigate = useNavigate();
 
-    return (
-        <div
-            className="Pas_Gilet_Jaune"
-            style={{
-            backgroundImage: `url('/fond_coeur.png')`, 
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            }}
-        >   
-            <div className="titre-container" style={{ position: 'relative' }}>
-                <img 
-                        src="/titre_bravo.png" 
-                        alt="titre_bravo.png" 
-                        className="titre_bravo"
-                />
-                <img 
-                            src="/emoji_pouce.png" 
-                            alt="emoji_pouce.png" 
-                            className="emoji_pouce"
-                />
-            </div>
+  return (
+    <div
+      className="Pas_Gilet_Jaune"
+      style={{
+        backgroundImage: `url('/fond_coeur.png')`, 
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >   
+      <div className="titre-container" style={{ position: 'relative' }}>
+        <img 
+          src="/titre_bravo.png" 
+          alt="titre_bravo.png" 
+          className="titre_bravo"
+        />
+        <img 
+          src="/emoji_pouce.png" 
+          alt="emoji_pouce.png" 
+          className="emoji_pouce"
+        />
+      </div>
 
-            <div className="macron-container" style={{ position: 'relative' }}>
-                <img 
-                            src="/macron_heureux.png" 
-                            alt="macron_heureux.png" 
-                            className="macron_heureux"
-                />
-                
-                <img 
-                            src="/marcon popelup.png" 
-                            alt="macron_popelup.png" 
-                            className="macron_popelup"
-                />
-            </div> 
-            <button onClick={() => window.location.reload()} className="button-succes">
-            Continuer vers l'inscription
-            </button>
-
-        </div>
-
-    )
+      <div className="macron-container" style={{ position: 'relative' }}>
+        <img 
+          src="/macron_heureux.png" 
+          alt="macron_heureux.png" 
+          className="macron_heureux"
+        />
+        <img 
+          src="/marcon popelup.png" 
+          alt="macron_popelup.png" 
+          className="macron_popelup"
+        />
+      </div> 
+      <button onClick={() => navigate('/auth')} className="button-succes">
+        Continuer vers l'inscription
+      </button>
+    </div>
+  );
 }
 
-export default  Gentil;
+export default Gentil;
