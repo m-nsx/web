dc506c1737862e5816ff5ec3d071308b8a29fea3
Titouan <verdier.titouan@gmail.com>
2025-04-10 08:49:04 +0200

    removed vertical lines in navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -55,14 +55,14 @@ function App() {
             <span>Bienvenue au Royaume Giratoire de France</span>
           </div>
           <nav>
-            <button onClick={() => navigate('/')} className="nav-button">Accueil</button> | 
-            <button onClick={() => navigate('/contact')} className="nav-button">Contact</button> | 
+            <button onClick={() => navigate('/')} className="nav-button">Accueil</button>
+            <button onClick={() => navigate('/contact')} className="nav-button">Contact</button>
             <button onClick={() => navigate('/leader')} className="nav-button">Notre idole</button>
-            {!token && <> | <button onClick={handleAuthClick} className="nav-button">Authentification</button></>}
+            {!token && <> <button onClick={handleAuthClick} className="nav-button">Authentification</button></>}
             {token && <>
-              | <button onClick={() => navigate('/vote')} className="nav-button">Vote</button> | 
-              <button onClick={() => navigate('/votes-management')} className="nav-button">Gestion des Votes</button> | 
-              <button onClick={() => navigate('/account')} className="nav-button">Mon Compte</button> | 
+              <button onClick={() => navigate('/vote')} className="nav-button">Vote</button>
+              <button onClick={() => navigate('/votes-management')} className="nav-button">Gestion des Votes</button>
+              <button onClick={() => navigate('/account')} className="nav-button">Mon Compte</button>
               <button onClick={handleLogout} className="nav-button">Déconnexion</button>
             </>}
           </nav>
